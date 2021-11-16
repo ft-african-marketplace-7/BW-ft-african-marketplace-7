711a512f5fd95c7a926e2cf844e1b3b85839e119
addybeuch <addybeuch@gmail.com>
2021-11-16 13:06:43 -0700

    items table created and migrated

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,20 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
+    .createTable("users", (users) => {
+      users.increments("user_id");
+      users.string("username", 200).notNullable();
+      users.string("password", 200).notNullable();
+      users.timestamps(false, true);
     })
-}
+    .createTable("items", (items) => {
+      items.increments("item_id");
+      items.string("name", 200).notNullable();
+      items.string("description", 200).notNullable();
+      items.integer("price", 200).notNullable();
+    });
+};
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
-}
+  await knex.schema.dropTableIfExists("items");
+  await knex.schema.dropTableIfExists("users");
+};
